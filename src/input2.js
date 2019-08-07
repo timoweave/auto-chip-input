8c30d945c04de36fbf57a4b126981c09d2162e77
Timothy Shiu <timoweave@gmail.com>
2019-08-07 12:00:46 -0700

    fix typo and unused variable

--- src/input2.js
+++ src/input2.js
@@ -1,71 +1,68 @@
-/* 
- NOTE: adapt code from https://material-ui.com/components/autocomplete/#react-select
-*/
 import React from "react";
 import PropTypes from "prop-types";
 import Select from "react-select";
 
 const AutoChipInput2 = props => {
-	const { title, placeholder, isMulti } = props;
-	const { search, setSearch, suggestedList, selectedList } = props;
-	const { setSelectedList, onSelectedList } = props;
+  const { placeholder, isMulti } = props;
+  const { search, setSearch, suggestedList, selectedList } = props;
+  const { setSelectedList, onSelectedList } = props;
 
-	const onChange = value => {
-		setSelectedList(value);
-		onSelectedList(value);
-	};
+  const onChange = value => {
+    setSelectedList(value);
+    onSelectedList(value);
+  };
 
-	return (
-		<Select
-			inputId="react-select-multiple"
-			placeholder={placeholder}
-			value={selectedList}
-			onChange={onChange}
-			inputValue={search}
-			onInputChange={setSearch}
-			options={suggestedList}
-			isMulti={isMulti}
-			disabled={true}
-		/>
-	);
+  return (
+    <Select
+      inputId="react-select-multiple"
+      placeholder={placeholder}
+      value={selectedList}
+      onChange={onChange}
+      inputValue={search}
+      onInputChange={setSearch}
+      options={suggestedList}
+      isMulti={isMulti}
+      disabled={true}
+    />
+  );
 };
 
 AutoChipInput2.propTypes = {
-	title: PropTypes.string,
-	placeholder: PropTypes.string,
-	isMulti: PropTypes.bool,
-	search: PropTypes.String,
-	suggestedList: PropTypes.arrayOf(
-		PropTypes.shape({
-			label: PropTypes.string,
-			value: PropTypes.string
-		})
-	),
-	selectedList: PropTypes.arrayOf(
-		PropTypes.shape({
-			label: PropTypes.string,
-			value: PropTypes.string
-		})
-	),
-	setSelectedList: PropTypes.func,
-	setSearch: PropTypes.func,
-	isLoading: PropTypes.bool,
-	onSelectedList: PropTypes.func,
-	disabled: PropTypes.bool
+  title: PropTypes.string,
+  placeholder: PropTypes.string,
+  isMulti: PropTypes.bool,
+  search: PropTypes.string,
+  suggestedList: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string
+    })
+  ),
+  selectedList: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string
+    })
+  ),
+  setSelectedList: PropTypes.func,
+  setSearch: PropTypes.func,
+  isLoading: PropTypes.bool,
+  onSelectedList: PropTypes.func,
+  disabled: PropTypes.bool
 };
 
 AutoChipInput2.defaultProps = {
-	title: "Search Options",
-	placeholder: "Enter option",
-	isMulti: false,
-	isLoading: false,
-	disabled: true,
-	onSelectedList: value => {
-		console.warn({
-			message: "provide your own callback",
-			value
-		});
-	}
+  title: "Search Options",
+  placeholder: "Enter option",
+  isMulti: false,
+  isLoading: false,
+  disabled: true,
+  onSelectedList: value => {
+    console.warn({
+      message: "provide your own callback",
+      value
+    });
+  }
 };
 
 export default AutoChipInput2;
--- src/omdb.js
+++ src/omdb.js
@@ -2,37 +2,40 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const fetchMovie = async props => {
-	const { search, setSuggestedList, setIsLoading, apiPrefix } = props;
-	setIsLoading(true);
-	const resp = await axios.get(`${apiPrefix}&s=${search}`);
-	const Search = resp.data.Search.map(movie => ({
-		// adapter the data for react-suggest
-		...movie,
-		label: movie.Title,
-		value: movie.Title
-	}));
-	setSuggestedList(Search);
-	setIsLoading(false);
+  const { search, setSuggestedList, setIsLoading, apiPrefix } = props;
+  setIsLoading(true);
+  const resp = await axios.get(`${apiPrefix}&s=${search}`);
+  setIsLoading(false);
+  if (!resp.data || !resp.data.Search) {
+    return;
+  }
+  const Search = resp.data.Search.map(movie => ({
+    // adapter the data for react-suggest
+    ...movie,
+    label: movie.Title,
+    value: movie.Title
+  }));
+  setSuggestedList(Search);
 };
 
 export const useOmdb = props => {
-	const { i, apiKey } = props;
-	const [search, setSearch] = useState("");
-	const [selectedList, setSelectedList] = useState([]);
-	const [suggestedList, setSuggestedList] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	useEffect(() => {
-		const apiPrefix = `https://www.omdbapi.com/?i=${i}&apiKey=${apiKey}`;
-		fetchMovie({ apiPrefix, search, setSuggestedList, setIsLoading });
-	}, [i, apiKey, search, setSuggestedList]);
+  const { i, apiKey } = props;
+  const [search, setSearch] = useState("");
+  const [selectedList, setSelectedList] = useState([]);
+  const [suggestedList, setSuggestedList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const apiPrefix = `https://www.omdbapi.com/?i=${i}&apiKey=${apiKey}`;
+    fetchMovie({ apiPrefix, search, setSuggestedList, setIsLoading });
+  }, [i, apiKey, search, setSuggestedList]);
 
-	return {
-		search,
-		suggestedList,
-		selectedList,
-		setSearch,
-		setSelectedList,
-		setSuggestedList,
-		isLoading
-	};
+  return {
+    search,
+    suggestedList,
+    selectedList,
+    setSearch,
+    setSelectedList,
+    setSuggestedList,
+    isLoading
+  };
 };
